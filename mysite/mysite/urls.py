36e667a3ed227531955a5044ed487c55079e6a0a
s0ooo0k <izz_m2@naver.com>
2020-08-01 07:04:31 +0900

    second edit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
     path('blog/<int:blog_id>', blog.views.detail, name="detail"),
+    path('blog/new/', blog.views.new, name="new"),
+    path('blog/create/', blog.views.create, name="create"),
+    path('blog/delete/<int:blog_id>', blog.views.delete, name="delete"),
+    path('blog/edit/<int:blog_id>', blog.views.edit, name="edit"),
+    path('blog/update/<int:blog_id>', blog.views.update, name="update"),
 ]
